db121566f50596a1db9436eb89f51e44eb6e35cb
Inderway <47097657+Inderway@users.noreply.github.com>
2022-01-06 21:51:34 +0800

    u

--- shells/rosiCatcher.py
+++ shells/rosiCatcher.py
@@ -2,39 +2,60 @@
 # for catching pictures of ROSI
 # -*- coding=utf8 -*-
 
-from selenium import webdriver
-import time 
-import os 
-import random
-from urllib import request
+
+from urllib import request, parse
 import re
+from selenium import webdriver
+import time
 
 class ROSIpics:
-    def __init__(self):
-        path="E:\Program Files (x86)\chromedriver\chromedriver.exe"
-        self.wb=webdriver.Chrome(executable_path=path)
-        self.wb.get(
-            "http://www.rosimm3.com/disi_mm/2862.html"
-        )
-        self.pics_url={}
+    def __init__(self, url):
+        self.url=url
 
+    def open_url(self, url):
+        req=request.Request(url)#实例化Request对象
+        #添加header
+        req.add_header('User-Agent', "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36")
+        #visit url, move the web page's binary data to page
+        page=request.urlopen(req)
+        #turn content of page into utf-8
+        self.html=page.read().decode('utf-8') 
 
-    def getPages(self):
-        page_text=self.wb.find_element_by_xpath(
-            "/html/body/section/div/div/div[1]/li[1]/a").text
-        self.page=int(page_text[1])
-
-    def getPicURL(self):
-        article_class=self.wb.find_element_by_xpath(
-            "/html/body/section/div/div/article"
-        ).text
-        print(article_class)
+    def get_img(self):
+        self.open_url(self.url)
+        #get pages
+        p=r'<a>共(\d+)页: </a>'
+        pages=int(re.findall(p,self.html)[0])
+        count=1
+        for i in range(pages):
+            #match images
+            #p=r'<img class="img-responsive" src="([^"]+\.jpg)"'
+            p=r'<img src=\'([^\']+\.jpg)\' alt='
+            imglist=re.findall(p,self.html)
+            #print(imglist)
+            for each in imglist:
+                filename=each.split("/")[-1]
+                photo=request.urlopen('http://www.rosimm3.com'+each)
+                w=photo.read()
+                f=open("E:\download\image\\"+str(count)+filename,'wb')
+                f.write(w)
+                f.close()
+                print(count)
+                count+=1
+            if(i==pages-1):
+                break
+            #get url of next page
+            p=r'<a href=\'([^\']+\.html)\'>下一页</a>'
+            next_url="http://www.rosimm3.com/disi_mm/"+re.findall(p, self.html)[0]
+            print(next_url)
+            self.open_url(next_url)
+            time.sleep(1)
 
 
 def main():
-    rosipic=ROSIpics()
-    rosipic.getPages()
-    rosipic.getPicURL()
+    rosipic=ROSIpics("http://www.rosimm3.com/disi_mm/1486.html")
+    rosipic.get_img()
 
 if __name__=='__main__':
     main()
