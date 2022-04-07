7b2baadd32311cc07675594aa5d6e68652f31499
Inderway <47097657+Inderway@users.noreply.github.com>
2022-04-07 14:53:06 +0800

    c

--- shells/renamer.py
+++ shells/renamer.py
@@ -3,15 +3,15 @@
 # -*- coding=utf8 -*-
 import os
 
-#dir_path='G:/Philosophy/pic/71/'
-#dir_path='G:/Philosophy/pic/cos8/'
-#dir_path='G:/Philosophy/comic/CGs/CG16/'
-#dir_path='F:/philosophy/temp/'
-dir_path='E:/download/image/'
+dir_path='G:/Philosophy/pic/84/'
+
+#dir_path='G:/Philosophy/comic/CGs/CG25/'
+#dir_path='E:/download/image/'
 # for i in range(10,11):
 #     affix=str(i)+'/'
 #     dir_path=path+affix
 #     print(i)
+
 for pic in os.listdir(dir_path):
     src_name=pic.split('.')[0]
     index=(src_name.split('(')[1]).split(')')[0]
