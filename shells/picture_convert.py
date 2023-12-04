cb353a7b6e7cd5ecc30da80263ff4744e2f388a7
Inderway <47097657+Inderway@users.noreply.github.com>
2023-12-04 13:19:37 +0800

    Create picture_convert.py

---
+++ shells/picture_convert.py
@@ -0,0 +1,25 @@
+# For converting the format of pictures
+# created by wei
+# Aug 8, 2023
+
+from PIL import Image
+import os
+
+path='F:/philosophy/nor/'
+for pic in os.listdir(path):
+    format=pic.split('.')[-1]
+    if format=='webp':
+        idx=pic.split('(')[-1].split(')')[0]
+        if len(idx)<3:
+            if len(idx)<2:
+                idx='00'+idx
+            else:
+                idx='0'+idx
+        os.rename(path+pic,path+'webp'+idx+'.webp')
+for pic in os.listdir(path):
+    format=pic.split('.')[-1]
+    name=pic.split('.')[0]
+    if format=='webp':
+        idx=pic.split('(')[-1].split(')')[0]
+        im = Image.open(path+pic).convert("RGB")
+        im.save(f"{path}{name}.jpg", "jpeg")
